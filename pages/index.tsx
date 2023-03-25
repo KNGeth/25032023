c839e42efae664e837f3e0cfc8551bd7e4f4869a
KNGeth <123255231+KNGeth@users.noreply.github.com>
2023-03-25 10:50:54 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ import styles from "../styles/Theme.module.css";
 import { parseIneligibility } from "../utils/parseIneligibility";
 
 // Put Your NFT Drop Contract address from the dashboard here
-const myNftDropContractAddress = "0xbC044bc063F4F88e9d52D833c200aE05Ea65FAF9";
+const myNftDropContractAddress = "0xEd9fb9A206D2eE3178d2d6b791Aeb22305345573";
 
 const Home: NextPage = () => {
   const { contract: nftDrop } = useContract(myNftDropContractAddress);
